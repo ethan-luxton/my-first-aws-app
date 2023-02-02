c081cdb8eb99c81700050bb663bbd8616bd05333
ethan-luxton <luxtonethan@hotmail.com>
2023-02-02 03:56:03 +0000

    Push

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-const { app } = require("express");
+const express = require("express");
+const app = express()
 
 app.get('/', (req, res) => {
     res.status(200).send('Hello World');
 });
-
-
 
 app.listen(3001, () => {
     console.log("Listing on port 3001");
